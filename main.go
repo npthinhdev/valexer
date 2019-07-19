2f4245f34baf23e95d9ffd2c8a217d36f3d35a69
Nguyen Phuoc Thinh <npthinh1996@gmail.com>
2019-07-19 17:49:30 +0700

    add struct to server

--- main.go
+++ main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/npthinhdev/valexer/internal/app"
 )
 
+type srvConfig struct {
+	DB struct {
+		Type string
+	}
+	HTTP struct {
+		Address           string
+		Port              int
+		ReadTimout        time.Duration
+		WriteTimeout      time.Duration
+		ReadHeaderTimeout time.Duration
+	}
+}
+
 func main() {
-	r := app.NewRouter()
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		panic(err)
+	var conf srvConfig
+
+	conf.HTTP.Address = "127.0.0.1"
+	conf.HTTP.Port = 8080
+	addr := fmt.Sprintf("%s:%d", conf.HTTP.Address, conf.HTTP.Port)
+	router := app.NewRouter()
+
+	httpServer := http.Server{
+		Addr:              addr,
+		Handler:           router,
+		ReadTimeout:       conf.HTTP.ReadTimout,
+		WriteTimeout:      conf.HTTP.WriteTimeout,
+		ReadHeaderTimeout: conf.HTTP.ReadHeaderTimeout,
 	}
 
+	log.Println("starting HTTP server...")
+	if err := httpServer.ListenAndServe(); err != nil {
+		log.Panic("http.ListenAndServe() error:", err)
+	}
 }
